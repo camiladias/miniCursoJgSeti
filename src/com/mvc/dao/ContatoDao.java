18b996c9023ad3691d355a4ed1a03e94c3dce531
Camila Dias <Camila Dias>
2016-10-09 21:06:45 -0300

    meta inf com persistence.xml

--- src/com/mvc/dao/ContatoDao.java
+++ src/com/mvc/dao/ContatoDao.java
@@ -1,5 +1,31 @@
 package com.mvc.dao;
+import javax.persistence.*;
 
 public class ContatoDao {
+	
+	private static ContatoDao instance;
+	protected EntityManager entityManager;
+
+	private ContatoDao() {
+		entityManager = getEntityManager();
+	}
+
+	public static ContatoDao getInstance(){
+		if (instance == null){
+			instance = new ContatoDao();
+		}
+		return instance;
+	}
+	
+	/*getEntityManager() é responsável por criar uma instância de EntityManager.*/
+	private EntityManager getEntityManager() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("agenda");
+		if (entityManager == null) {
+			entityManager = factory.createEntityManager();
+		}
+		return entityManager;
+	}
+	
+	
 
 }
--- src/com/mvc/model/Contato.java
+++ src/com/mvc/model/Contato.java
@@ -1,5 +1,52 @@
 package com.mvc.model;
+import javax.persistence.*;
 
+@Entity
+@Table(name="contato")
 public class Contato {
-
+	
+	@Id
+	@Column(name="id",nullable=false,unique=true)
+	@GeneratedValue
+	private Integer id;
+	@Column(name="nome", length=64,nullable=false)
+	private String nome;
+	@Column(name="telefone", length=64,nullable=false)
+	private String telefone;
+	@Column(name="email", length=64,nullable=false)
+	private String email;
+		
+	public Contato(int id, String nome, String telefone, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.telefone = telefone;
+		this.email = email;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
