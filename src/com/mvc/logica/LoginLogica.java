7452eb46732a3d0dad3ba5c1bd554f50698747c6
Thiago Vilela <thiagovilela@petasoft.com>
2016-10-19 13:25:41 -0200

    Commit #12 - Ajuste da classe LoginLogica

--- src/com/mvc/logica/LoginLogica.java
+++ src/com/mvc/logica/LoginLogica.java
@@ -5,8 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/LoginLogica")
-public class LoginLogica extends HttpServlet implements Logica {
+public class LoginLogica implements Logica {
 
 	/**
 	 * 
