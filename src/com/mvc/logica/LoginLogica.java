423b4ea957983706e2aa5f0344613c7def330a85
Thiago Vilela <thiagovilela@petasoft.com>
2016-10-19 13:34:08 -0200

    Commit #14 - Ajustes do Merge

--- src/com/mvc/logica/LoginLogica.java
+++ src/com/mvc/logica/LoginLogica.java
@@ -1,21 +1,12 @@
 package com.mvc.logica;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-<<<<<<< HEAD
+
 public class LoginLogica implements Logica {
-=======
-public class LoginLogica extends HttpServlet implements Logica {
->>>>>>> 4340cb3c36b491fc46b82e624fa74526fca55786
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-@Override
+	
+	@Override
 	public String executa(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
 		if ((req.getParameter("idUsuario").equals("admin")) && (req.getParameter("senhaUsuario").equals("admin"))) {
